0bdc2e96d6d813253c5c708c29a477a3b504f2e2
ramhaidar <49301219+ramhaidar@users.noreply.github.com>
2023-11-29 01:49:32 +0700

    0.0.7
    Update: Improved IGracias Automate Survey Script
    
    This commit includes the following changes:
    - Updated the script version from 0.0.6 to 0.0.7.
    - Refactored the code for auto-filling radio buttons to make it more efficient and readable.
    - Simplified the code for auto-clicking the save, update, or submit button.
    
    These changes aim to improve the efficiency and readability of the script.

--- IGracias_Automate_Survey.user.js
+++ IGracias_Automate_Survey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IGracias Automate Survey
 // @namespace    http://tampermonkey.net/
-// @version      0.0.6
+// @version      0.0.7
 // @updateURL    https://github.com/ramhaidar/IGracias_Automate_Survey/raw/main/IGracias_Automate_Survey.user.js
 // @downloadURL  https://github.com/ramhaidar/IGracias_Automate_Survey/raw/main/IGracias_Automate_Survey.user.js
 // @description  Auto Survey Script!
@@ -12,51 +12,28 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js
 // ==/UserScript==
 
-// Auto Fill Radio Button
-;(function () {
+(function () {
     'use strict'
 
-    var kepuasan = 'Puas' //Sangat puas | Puas | Cukup | Tidak puas | Sangat tidak puas
-    var yes_or_no = 'Ya' // Ya | Tidak
-    var persetujuan = 'Setuju'
+    const responses = ['Puas', 'Ya', 'Setuju']; //Sangat puas | Puas | Cukup | Tidak puas | Sangat tidak puas
 
-    $(
-        '.answerlist1:contains(' +
-        kepuasan +
-        '), .answerlist1:contains(' +
-        yes_or_no +
-        '), .answerlist1:contains(' +
-        persetujuan +
-        ')'
-    ).each(function () {
-        $(this)
-            .parent()
-            .each(function () {
-            $(this).find('.answerlist2').children().click()
-        })
-    })
-})()
+    responses.forEach(response => {
+        $(`.answerlist1:contains(${response})`).each(function () {
+            $(this).parent().find('.answerlist2').children().click();
+        });
+    });
 
-// Empty Text on TextArea Form
-$('textarea').val('‎ ')
+    $('textarea').val('‎ '); // Empty Text on TextArea Form
 
-/*
-<input class="floatL3" name="button" type="image" value="simpan" src="../images/btn_save2.png" alt="Submit button"> // Save
-<input class="floatL3" name="button" type="image" value="simpan" src="../images/btn2.png" alt="Submit button"> // Update
-<input class="floatL4" name="button" type="image" value="simpan" src="../images/btn_submit.gif" alt="Submit button"> // Submit
-*/
+    const srcToSelect = "../images/btn_save2.png"; // Save Button
 
-const srcToSelect = "../images/btn_save2.png"; // Save Button
-
-// Auto Click Save or Update or Submit Button
-$(document).ready(function () {
-    var doc = $(document).find('input')
-    doc.each(function () {
-        if ($(this).val() == 'simpan') {
+    // Auto Click Save or Update or Submit Button
+    $(document).ready(function () {
+        $(document).find('input[value="simpan"]').each(function () {
             const elements = document.querySelectorAll(`input[type="image"][src="${srcToSelect}"]`);
             elements.forEach(element => {
                 element.click();
             });
-        }
-    })
-})
+        });
+    });
+})();
